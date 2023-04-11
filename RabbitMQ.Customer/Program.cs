23da050a7c4fff2a4b7a563a03e8d241cda2dabb
sfatektas <sfatektas55@hotmail.com>
2023-04-11 15:42:04 +0300

    Add project files.

---
+++ RabbitMQ.Customer/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Bağlantı Oluşturma 
+
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+//Bağlantı Oluşturma 
+ConnectionFactory factory = new ConnectionFactory();
+factory.Uri = new Uri("amqps://xamohava:spM7OwkS3NfNFbE9e8K5naFn_UW3fPWq@shrimp.rmq.cloudamqp.com/xamohava");
+
+//Bağlantı Aktifleştirme kanal açma 
+using IConnection connection = factory.CreateConnection(); //disposaable bir nesne
+using IModel channel = connection.CreateModel();
+
+//Queue Oluşturma
+channel.QueueDeclare(queue: "example-messageQueue", exclusive: false);
+//exclusive değeri publisher ile aynı yapılandırmada olmalı.
+
+//ilgili channele tanımlı bir event tanımlıyoruz.
+EventingBasicConsumer consumer = new(channel);
+channel.BasicConsume(queue: "example-messageQueue", autoAck: true, consumer);
+
+//Kuyruğa yeni bir mesaj geldiğinde çalışacak olan fonksiyon.
+//consumer.Received += (sender, e) =>
+//{
+//    Console.WriteLine($"{Encoding.UTF8.GetString(e.Body.Span.ToArray())}");
+//};
+
+//Bir diğer event kullanımı
+consumer.Received += HandleMessage;
+void HandleMessage(object sender, BasicDeliverEventArgs e)
+{
+    Console.WriteLine($"{Encoding.UTF8.GetString(e.Body.Span.ToArray())}");
+}
+Console.Read();
---
+++ RabbitMQ.Publisher/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using RabbitMQ.Client;
+using System.Text;
+
+/* // Publisher işlem sırası ; 
+ * Bağlantı oluşturma (RabbitMQ sunucusuna bağlanmak)
+ * Bağlantı Aktifleştirme ve Kanal Açma (Bağlandıktan sonra işlemleri yönetebilmek için kanal açmak)
+ * Queue Oluşturma
+ * Queue'ye Mesaj Gönderme
+ * 
+ */
+
+//Bağlantı Oluşturm işlemi 
+ConnectionFactory factory = new ConnectionFactory();
+factory.Uri = new("amqps://xamohava:spM7OwkS3NfNFbE9e8K5naFn_UW3fPWq@shrimp.rmq.cloudamqp.com/xamohava");
+
+//Bağlantıyı Aktşfleştirme ve channel açma 
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+//Queue oluşturma 
+channel.QueueDeclare("example-messageQueue", exclusive: false); // excluesive sadece bu bağlantıya özel bağlantı sağlar , true olarak atanırsa farklı bir consumerdan kuyruğa erişemeyeceğiz.
+
+//Queue'ye mesajı publish etme 
+
+byte[] message = Encoding.UTF8.GetBytes("First Publish message");
+
+channel.BasicPublish(exchange: "", routingKey: "example-messageQueue", body: message);
+Console.WriteLine("message publish edildi.");
+
+//eğer default olarak exchange boş geçilir ise rabbitMQ direct exchange yaklaşımı kabul eder.
+//direct exchange yapısında routuing key değeri queue ismine denk gelir.
+
+Console.Read();
